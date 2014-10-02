18cca468c1a30041ab442a9ddcd367a8519c0299
Merili <meriliasu@gmail.com>
2014-10-02 20:29:26 +0300

    Ex 4.2
    Ex 4.2

--- layouts/exercises.php
+++ layouts/exercises.php
@@ -25,9 +25,11 @@
               }
           ?> 
         <h2>Juhuslik pilt</h2>
+         <?php
+            echo '<img src="http://users.khk.ee/kaspar.naaber/svp/periood-1/arvestus/images/'.rand(1,12).'.jpg">';
+          ?>
 
         <h2>Kõikide piltide väljastamine</h2>
-        <!--  Sisesta siia kood -->
       </section>
     </main>
 
